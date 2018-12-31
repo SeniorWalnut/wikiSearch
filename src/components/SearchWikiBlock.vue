98025323b6dc6ae36dfe0a5caeec1bfaf9ada876
agafonov <agafonov@etm-system.ru>
2018-12-31 16:43:01 +0200

    Done

--- src/components/SearchWikiBlock.vue
+++ src/components/SearchWikiBlock.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app">
-  		<div class="SearchWikiBlock">
-  			<div class="SearchWikiBlock__title">{{title}}</div>
-  			<div class="SearchWikiBlock__info">{{info}}</div>
-  			<a :href="link" class="SearchWikiBlock__link">Link to page -></a>
-  		</div>
-  </div>
+	<div class="SearchWikiBlock" :style="{borderColor: activeColor}"> 
+		<div class="SearchWikiBlock__title">{{title}}</div>
+		<div class="SearchWikiBlock__info"><span v-html="info"></span></div>
+		<a :style="{color: activeColor}" :href="link" class="SearchWikiBlock__link">Link to page <i class="fas fa-arrow-right"></i></a>
+	</div>
 </template>
 
 <script>
+const colors = [
+	"#96CEB4",
+	"#FFCC5C",
+	"#FF6F69",
+	"#FFEEAD",
+	"#96CEB4"
+]
+
 export default {
 	props: {
 		page: Object
@@ -17,12 +23,36 @@ export default {
 		return {
 			title: this.page.title,
 			info: this.page.snippet,
-			link: "https://en.wikipedia.org/?curid=" + this.page.pageid
+			link: "https://en.wikipedia.org/?curid=" + this.page.pageid,
+			activeColor: colors[Math.floor(Math.random() * colors.length)]
 		}
 	}
 };
 </script>
 
-<style>
-
+<style lang="sass">
+	.SearchWikiBlock
+		padding: 15px
+		width: 50%
+		// border-right: 3px solid 
+		margin: 0 auto
+		margin-bottom: 30px 
+		&:hover
+			border-left: 5px solid
+			transition: border-left .4s
+		&__title
+			font-size: 24px
+			padding-bottom: 10px
+			font-family: 'Aleo', serif
+		&__info
+			font-size: 14px
+			font-family: Roboto Mono
+			padding-bottom: 10px
+		&__link
+			text-decoration: none
+			font-family: Raleway
+			// color: #96CEB4
+			& i 
+				padding-left: 10px
+				font-size: 12px
 </style>
--- src/components/SearchWikiList.vue
+++ src/components/SearchWikiList.vue
@@ -1,14 +1,16 @@
 <template>
 	<div class="SearchWikiList">
 		<div class="SearchWikiList__container">
-			<div class="SeachWikiList__title">SearchWiki</div>
-			<form class="SearchWikiList__form SearchWikiList-form">
-				<div class="SearchWikiList-form__title">Input something to find:</div>
-				<input v-model.trim="query" type="text" class="SearchWikiList-form__input"><br>
-				<button type="button" @click="sendQuery" class="SearchWikiList-form__button">Search</button>
-			</form>
-			<div class="SearchWikiList__list" v-for="page in pages">
-				<search-wiki-block :page="page"/>
+			<div class="SearchWikiList__form SearchWikiList-form" :class="{closed: isClosed}">
+				<div class="SearchWikiList__title">Search Wiki</div>
+				<input placeholder="Input something to find" v-model.trim="query" @keyup.enter="sendQuery" type="text" class="SearchWikiList-form__input SearchWikiList-form__input"><br>
+				<button type="button" @click="sendQuery" class="SearchWikiList-form__button">
+					<i v-if="isClosed" class="fas fa-search" @click="isClosed = !isClosed"></i>
+					<i v-else class="fas fa-times" @click="isClosed = !isClosed"></i>
+				</button>
+			</div>
+			<div class="SearchWikiList__list" v-for="(page, index) in pages">
+				<search-wiki-block :key="index" :page="page"/>
 			</div>
 		</div>
 	</div>
@@ -20,22 +22,32 @@ import SearchWikiBlock from './SearchWikiBlock';
 
 const url = "https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&utf8=&prop=links& format=json&srsearch=";
 
+
 export default {
 	name: "search-wiki-list",
+
 	data() {
 		return {
 			query: '',
-			pages: []
+			pages: [],
+			isClosed: true,
 		}
 	},
+
 	methods: {
 		sendQuery() {
-			if (this.query.length) {
+			this.pages = []
+			if (!this.isClosed && this.query.length) {
+				this.isClosed = false
+				this.finded = true
 				this.$http
 					.get(url + this.query)
-					.then(resp => (this.pages = resp.data.query.search))
-			}
+					.then(resp => {
+						this.pages = resp.data.query.search
+					})
+			} 
 		}
+
 	},
 	components: {
 		SearchWikiBlock
@@ -45,10 +57,59 @@ export default {
 </script>
 
 
-<style>
-	.SearchWikiList {
-		width: 100%;
-		display: flex;
-		justify-content: center;
-	}
+<style lang="sass">
+	.SearchWikiList 
+		padding: 20px
+		&__container 
+			display: flex
+			justify-content: center
+			align-items: center
+			flex-flow: column
+
+		&__title
+			font-family: 'Aleo', serif
+			font-size: 56px
+			letter-spacing: 5px
+			word-spacing: 15px
+			margin-right: 10px
+			// margin-right: 20px
+
+		&-form
+			display: flex
+			flex-direction: row
+			&.closed 
+				.SearchWikiList-form 
+					&__input
+						width: 0
+						border: 0
+						padding: 0
+						transition: all .5s
+					&__button
+						margin-left: 0
+						transition: all .3s
+						
+			&__input
+				border-radius: 50px
+				outline: none
+				border: 1px solid #202020
+				padding: 12px 50px
+				padding-left: 20px
+				transition: all .3s
+				font-family: 'Roboto Mono', monospace
+				&::placeholder
+					font-family: 'Aleo', serif
+
+			&__button
+				border: none
+				background: none
+				font-size: 40px
+				outline: 0
+				cursor: pointer
+				margin-left: -60px
+				& i:hover
+					color: #96CEB4
+					font-size: 45px
+					transition: all .3s
+		&__list 
+			margin-top: 20px
 </style>
